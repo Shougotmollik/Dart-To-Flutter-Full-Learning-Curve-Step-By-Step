122b6feae73cf69b69e2c826d28a6fec650206f8
Shougotmollik <Shougotmollik@gmail.com>
2023-05-23 21:37:21 +0600

    add hello wold

---
+++ data_type_and_variables.dart
@@ -0,0 +1,31 @@
+void main() {
+//string
+
+  String name = "jhon";
+  var city = "tokeyo";
+  print(name);
+  print(city);
+
+//integer
+  int age = 21;
+  print(age);
+  var x = 221; //it is inferred as integer automatically by compiler
+  print(x);
+
+//double
+
+  double percentage = 93.4;
+  print(percentage);
+  var temparature = 13.56;
+  print(temparature);
+
+//Boolean
+
+  bool isValid = true;
+  var isAlive = false;
+  print(isValid);
+  print(isAlive);
+
+  //Note: All data types in Dart are objects.
+  //Therefore ,their initial value is by deafult "null"
+}
