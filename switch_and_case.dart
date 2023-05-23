a82e635e6efd77ecdd82c53c60d6caa0f9123a73
Shougotmollik <Shougotmollik@gmail.com>
2023-05-24 00:47:52 +0600

    add switch and case

---
+++ switch_and_case.dart
@@ -0,0 +1,26 @@
+import 'dart:ffi';
+
+void main() {
+  String grade = "A";
+
+  switch (grade) {
+    case "A":
+      print("Your grade is $grade");
+      break;
+
+    case "B":
+      print("Your grade is $grade");
+      break;
+    case "C":
+      print("Your grade is $grade");
+      break;
+    case "D":
+      print("Your grade is $grade");
+      break;
+    case "F":
+      print("You are fail");
+      break;
+    default:
+      print("Invalid Grade");
+  }
+}
