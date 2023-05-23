3549f0f324f5546055fe2956c8e94da7bf917163
Shougotmollik <Shougotmollik@gmail.com>
2023-05-23 22:20:00 +0600

    add const and final keyword

---
+++ const_and_final.dart
@@ -0,0 +1,18 @@
+import 'dart:ffi';
+
+void main() {
+// final keyword
+
+  final cityName = "Tokeyo";
+  final String CountryName = "Japan";
+
+  //constant keyword
+
+  const PI = 3.1416;
+  const double Gravity = 9.8;
+}
+
+class Circle {
+  final color = "Red";
+  static const PI = 3.1416;
+}
