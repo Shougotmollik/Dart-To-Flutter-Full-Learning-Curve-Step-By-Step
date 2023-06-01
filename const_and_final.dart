f18c507ad69e1d2a01cdb13da1422c25524adf13
Shougotmollik <Shougotmollik@gmail.com>
2023-06-01 16:11:23 +0600

    changes the file

--- const_and_final.dart
+++ const_and_final.dart
@@ -1,15 +1,17 @@
-import 'dart:ffi';
-
 void main() {
 // final keyword
 
   final cityName = "Tokeyo";
   final String CountryName = "Japan";
+  print(cityName);
+  print(CountryName);
 
   //constant keyword
 
   const PI = 3.1416;
+  print(PI);
   const double Gravity = 9.8;
+  print(Gravity);
 }
 
 class Circle {
--- stirng_interpolation.dart
+++ stirng_interpolation.dart
@@ -3,15 +3,21 @@ void main() {
   var isAdult = true;
   int age = 21;
   var my_name = "SHOUGOT MOllik";
+  print("may name is $my_name ."
+      "my age is $age."
+      "i am adult its $isAdult");
 
 // various ways to define Strings literals in dart
 
   String s1 = 'Single';
   String s2 = "Double";
   String s3 = 'It\'s easy';
+  print("All strings $s1$s2$s3");
 
   String long_sentance = 'Hi there, your just discover dart is awasome'
       'dart is very flexiable programming language';
+
+  print(long_sentance);
 
   //strings interpolation // use [${ ...}] from this [ 'some '+thing]
 
