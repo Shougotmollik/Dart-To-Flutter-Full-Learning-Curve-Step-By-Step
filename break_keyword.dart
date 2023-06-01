8a33767b4285c9002ab176f55bd3878735700156
Shougotmollik <Shougotmollik@gmail.com>
2023-06-01 16:25:14 +0600

    add break keyword

--- break_keyword.dart
+++ break_keyword.dart
@@ -1,1 +1,11 @@
-void main() {}
+void main() {
+  // break keyword  using lebels
+
+  myOuterloop:
+  for (var i = 1; i <= 10; i++) {
+    print(i);
+    if (i == 5) {
+      break myOuterloop;
+    }
+  }
+}
