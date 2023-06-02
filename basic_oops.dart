dfcdbb7b64deceede02affff7355edfe2bf90b0d
Shougotmollik <Shougotmollik@gmail.com>
2023-06-02 18:30:39 +0600

    add basic pops file

---
+++ basic_oops.dart
@@ -0,0 +1,32 @@
+void main() {
+  student();
+}
+
+// class
+class Student {
+  var id = 01; //instance variable or local variable
+  String name = "shougot"; //instance variable or local variable
+
+  StudentId() {
+    // all student id
+  }
+
+  StudentName() {
+    //all student name
+  }
+
+  void StudentStudy() {
+    // All student study material
+  }
+}
+
+// Default constructor
+student() {
+  print("This is Default constractor ");
+}
+
+//parameter constructor
+// Student_parameter(var id, String name) {
+//   this.id = id;
+//   this.name = name;
+// }
