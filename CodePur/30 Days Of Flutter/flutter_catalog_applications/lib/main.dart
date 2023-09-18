214896a72f0fdfac72b6a07b6987051ae5cb893c
Shougotmollik <shougotmollik@gmail.com>
2023-09-19 00:07:29 +0600

    home page

--- CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/main.dart
+++ CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog_applications/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,9 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Container(
-        child: Text("Welcome To My App"),
-      ),
+      home: HomePage(),
     );
   }
 }
