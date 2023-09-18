2521d65808a26dc1c32e609205df830e4a577c76
Shougotmollik <shougotmollik@gmail.com>
2023-09-18 22:06:02 +0600

    day 3

--- CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/main.dart
+++ CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog_applications/home_page.dart';
+import 'package:flutter_catalog_applications/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
