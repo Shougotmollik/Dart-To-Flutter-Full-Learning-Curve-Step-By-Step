de9d75633ccc5404938c83ba96f4456060769231
Shougotmollik <shougotmollik@gmail.com>
2023-09-18 22:07:02 +0600

    day 3

--- CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/main.dart
+++ CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/main.dart
@@ -10,8 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:HomePage()
-    );
+    return MaterialApp(home: HomePage());
   }
 }
