4065ef5da2ddfb9f1f791f26c52a836a89a6e529
Shougotmollik <shougotmollik@gmail.com>
2023-09-21 16:13:08 +0600

    day 5

--- CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/main.dart
+++ CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog_applications/pages/home_page.dart';
 import 'package:flutter_catalog_applications/pages/login_page.dart';
+import 'package:flutter_catalog_applications/utils/routes.dart';
 //import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -20,8 +21,8 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(brightness: Brightness.dark),
       routes: {
         "/": (context) => LoginPage(),
-        "home": (context) => HomePage(),
-        "/login": (context) => LoginPage(),
+        MyRoutes.homeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => LoginPage(),
       },
     );
   }
