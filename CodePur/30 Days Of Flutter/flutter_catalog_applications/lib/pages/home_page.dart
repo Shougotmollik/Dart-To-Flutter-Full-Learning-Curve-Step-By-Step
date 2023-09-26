cb7b7bd26a0b10782ae02b6f5e03738eb7d28d46
Shougotmollik <shougotmollik@gmail.com>
2023-09-26 20:11:12 +0600

    integrated home page

--- CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/pages/home_page.dart
+++ CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/pages/home_page.dart
@@ -1,8 +1,34 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_catalog_applications/widgets/drawer.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_catalog_applications/models/catalog.dart';
+import 'package:flutter_catalog_applications/widgets/drawer.dart';
+import 'package:flutter_catalog_applications/widgets/item_widgets.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productsData = decodeData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +37,15 @@ class HomePage extends StatelessWidget {
         title: Container(child: Text("Catalog App")),
         centerTitle: true,
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to my app"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: CatalogModel.items.length,
+          itemBuilder: (context, index) {
+            return ItemWidgets(
+              item: CatalogModel.items[index],
+            );
+          },
         ),
       ),
       drawer: myDrawer(),
