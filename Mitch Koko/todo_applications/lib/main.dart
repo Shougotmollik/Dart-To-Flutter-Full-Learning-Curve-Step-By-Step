5fd2516bb89622d47605dd5d3880fe3b49046b30
Shougotmollik <shougotmollik@gmail.com>
2023-09-28 20:40:28 +0600

    create a new flutter project

---
+++ Mitch Koko/todo_applications/lib/main.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple[200],
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(20)),
+            padding: EdgeInsets.all(50),
+            child: Text(
+              "Shougot",
+              style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
