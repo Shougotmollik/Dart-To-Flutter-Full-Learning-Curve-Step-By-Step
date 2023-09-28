081e041026f846d9bb1851878cfca29e7da5821e
Shougotmollik <shougotmollik@gmail.com>
2023-09-28 13:46:55 +0600

    complete home page

--- CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/main.dart
+++ CodePur/30 Days Of Flutter/flutter_catalog_applications/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_catalog_applications/pages/home_page.dart';
 import 'package:flutter_catalog_applications/pages/login_page.dart';
 import 'package:flutter_catalog_applications/utils/routes.dart';
 import 'package:flutter_catalog_applications/widgets/theme.dart';
-import 'package:google_fonts/google_fonts.dart';
+ import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
