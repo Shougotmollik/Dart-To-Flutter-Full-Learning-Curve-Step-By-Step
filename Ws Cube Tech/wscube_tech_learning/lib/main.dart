2c3d74ac4743bec8b55a530a2d404ed458ddc066
Shougotmollik <shougotmollik@gmail.com>
2023-10-05 01:15:15 +0600

    update

--- Ws Cube Tech/wscube_tech_learning/lib/main.dart
+++ Ws Cube Tech/wscube_tech_learning/lib/main.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors, camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:wscube_tech_learning/pages/button_page.dart';
+import 'package:wscube_tech_learning/pages/home_page.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -13,30 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.yellow[200],
-        appBar: AppBar(
-          title: Text("WsCube Tech Flutter Basics",
-              style: TextStyle(color: Colors.black54)),
-          centerTitle: true,
-          elevation: 0.0,
-          backgroundColor: Colors.yellow[600],
-        ),
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-                color: Colors.amber[400],
-                borderRadius: BorderRadius.circular(20)),
-            child: Center(
-                child: Text(
-              "Hello This is text example",
-              style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic),
-            )),
-          ),
-        ),
-      ),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+      home: ButtonPage(),
     );
   }
 }
