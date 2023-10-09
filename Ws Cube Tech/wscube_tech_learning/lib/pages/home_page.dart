e11be6198632942a07cde9325bed279f244eaa10
Shougot mollik <shougotmollik@gmail.com>
2023-10-09 21:35:52 +0600

    updated

--- Ws Cube Tech/wscube_tech_learning/lib/pages/home_page.dart
+++ Ws Cube Tech/wscube_tech_learning/lib/pages/home_page.dart
@@ -8,36 +8,55 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow[200],
-      appBar: AppBar(
-        title: Text("WsCube Tech Flutter Basics",
-            style: TextStyle(color: Colors.black54)),
-        centerTitle: true,
-        elevation: 0.0,
-        backgroundColor: Colors.yellow[600],
-      ),
-      body: Center(
-        child: InkWell(
-          onTap: () {
-            print("Clicked on container");
-          },
-          child: Container(
-            height: 200,
-            width: 200,
-            decoration: BoxDecoration(
-                color: Colors.amberAccent,
-                borderRadius: BorderRadius.circular(20)),
-            child: Center(
-                child: Text(
-              "Click Here!!!",
-              style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w400,
-                  fontStyle: FontStyle.italic),
-            )),
-          ),
+        backgroundColor: Colors.yellow[200],
+        appBar: AppBar(
+          title: Text("WsCube Tech Flutter Basics",
+              style: TextStyle(color: Colors.black54)),
+          centerTitle: true,
+          elevation: 0.0,
+          backgroundColor: Colors.yellow[600],
         ),
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 12),
+                  height: 200,
+                  color: Colors.green,
+                ),
+                Container(
+                  height: 200,
+                  color: Colors.orange,
+                ),
+                Container(
+                  height: 200,
+                  color: Colors.deepPurple,
+                ),
+                Container(
+                  height: 200,
+                  color: Colors.white,
+                ),
+                Container(
+                  height: 200,
+                  color: Colors.redAccent,
+                ),
+                Container(
+                  height: 200,
+                  color: Colors.blueAccent,
+                ),
+                Container(
+                  height: 200,
+                  color: Colors.pink,
+                ),
+                Container(
+                  height: 200,
+                  color: Colors.black,
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
