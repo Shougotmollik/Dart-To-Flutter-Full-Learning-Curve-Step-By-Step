d896e72b603002c9a1a75ba723618f704737a3f2
Shougot mollik <shougotmollik@gmail.com>
2023-10-09 20:37:23 +0600

    updated

--- Ws Cube Tech/wscube_tech_learning/lib/pages/home_page.dart
+++ Ws Cube Tech/wscube_tech_learning/lib/pages/home_page.dart
@@ -7,29 +7,18 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        backgroundColor: Colors.yellow[200],
-        appBar: AppBar(
-          title: Text("WsCube Tech Flutter Basics",
-              style: TextStyle(color: Colors.black54)),
-          centerTitle: true,
-          elevation: 0.0,
-          backgroundColor: Colors.yellow[600],
-        ),
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-                color: Colors.amber[400],
-                borderRadius: BorderRadius.circular(20)),
-            child: Center(
-                child: Text(
-              "Hello This is text example",
-              style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic),
-            )),
-          ),
-        ),
-      );
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text("WsCube Tech Flutter Basics",
+            style: TextStyle(color: Colors.black54)),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.yellow[600],
+      ),
+      body: Center(
+        child: Container(),
+      ),
+    );
   }
 }
