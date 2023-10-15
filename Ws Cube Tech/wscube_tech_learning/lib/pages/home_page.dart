273b9d40f05c163fd30ee6212860be418bbe7da1
Shougot mollik <shougotmollik@gmail.com>
2023-10-15 12:16:14 +0600

    updated

--- Ws Cube Tech/wscube_tech_learning/lib/pages/home_page.dart
+++ Ws Cube Tech/wscube_tech_learning/lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace, sort_child_properties_last, non_constant_identifier_names
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace, sort_child_properties_last, non_constant_identifier_names, prefer_const_literals_to_create_immutables, avoid_print
 
 import 'package:flutter/material.dart';
 
@@ -24,48 +24,25 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: Colors.yellow[600],
         ),
         body: Center(
-            child: Container(
-          width: 350,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextField(
-                keyboardType: TextInputType.phone,
-                controller: UserInput,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.people_outline),
-                  hintText: "Enter Username",
-                  hintStyle: TextStyle(fontStyle: FontStyle.italic),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                ),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                controller: PasswordInput,
-                obscureText: true,
-                obscuringCharacter: "*",
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.enhanced_encryption_outlined),
-                    hintText: "Enter Password",
-                    hintStyle: TextStyle(fontStyle: FontStyle.italic),
-                    suffixIcon: IconButton(
-                        onPressed: () {}, icon: Icon(Icons.remove_red_eye)),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    )),
-              ),
-              SizedBox(height: 12),
+              Text("Select Date"),
               ElevatedButton(
-                onPressed: () {},
-                child: Text("Login"),
-                style: ButtonStyle(
-                  iconSize: MaterialStatePropertyAll(34),
-                ),
+                child: Text("Select"),
+                onPressed: () async {
+                  TimeOfDay? pickedTime = await showTimePicker(
+                      context: context,
+                      initialTime: TimeOfDay.now(),
+                      initialEntryMode: TimePickerEntryMode.dial);
+
+                  if (pickedTime != null) {
+                    print("${pickedTime.hour}:${pickedTime.minute}");
+                  }
+                },
               )
             ],
           ),
-        )));
+        ));
   }
 }
