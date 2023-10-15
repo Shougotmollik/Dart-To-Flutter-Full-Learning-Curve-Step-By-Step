8ecbf4702d68e85c79c35fee148345f123b72a8a
Shougot mollik <shougotmollik@gmail.com>
2023-10-15 10:50:58 +0600

    updated

--- Ws Cube Tech/wscube_tech_learning/lib/pages/home_page.dart
+++ Ws Cube Tech/wscube_tech_learning/lib/pages/home_page.dart
@@ -17,22 +17,31 @@ class HomePage extends StatelessWidget {
           backgroundColor: Colors.yellow[600],
         ),
         body: Center(
-            child: Card(
-                surfaceTintColor: Colors.blueAccent,
-                shadowColor: Colors.amberAccent[400],
-                elevation: 20,
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Container(
-                    child: Text(
-                      "Shougot Mollik",
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.italic,
-                          color: Colors.black54),
-                    ),
-                  ),
-                ))));
+            child: Container(
+          width: 350,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.people_outline),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    )),
+              ),
+              SizedBox(height: 8),
+              TextField(
+                decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                    suffixIcon: IconButton(
+                        onPressed: () {}, icon: Icon(Icons.remove_red_eye)),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    )),
+              ),
+            ],
+          ),
+        )));
   }
 }
