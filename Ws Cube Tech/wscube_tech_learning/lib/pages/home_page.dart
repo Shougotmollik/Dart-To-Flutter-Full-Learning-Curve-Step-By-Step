5d4a82150000bcd86ae726b82d9b4616148b13f0
Shougot mollik <shougotmollik@gmail.com>
2023-10-15 11:46:02 +0600

    updated

--- Ws Cube Tech/wscube_tech_learning/lib/pages/home_page.dart
+++ Ws Cube Tech/wscube_tech_learning/lib/pages/home_page.dart
@@ -1,10 +1,17 @@
-// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers, sized_box_for_whitespace, sort_child_properties_last, non_constant_identifier_names
 
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var UserInput = TextEditingController();
+  var PasswordInput = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,15 +30,24 @@ class HomePage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextField(
+                keyboardType: TextInputType.phone,
+                controller: UserInput,
                 decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.people_outline),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(16),
-                    )),
+                  prefixIcon: Icon(Icons.people_outline),
+                  hintText: "Enter Username",
+                  hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
               ),
               SizedBox(height: 8),
               TextField(
+                controller: PasswordInput,
+                obscureText: true,
+                obscuringCharacter: "*",
                 decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.enhanced_encryption_outlined),
                     hintText: "Enter Password",
                     hintStyle: TextStyle(fontStyle: FontStyle.italic),
                     suffixIcon: IconButton(
@@ -40,6 +56,14 @@ class HomePage extends StatelessWidget {
                       borderRadius: BorderRadius.circular(16),
                     )),
               ),
+              SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Login"),
+                style: ButtonStyle(
+                  iconSize: MaterialStatePropertyAll(34),
+                ),
+              )
             ],
           ),
         )));
