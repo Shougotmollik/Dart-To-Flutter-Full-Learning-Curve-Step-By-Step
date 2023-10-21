786b65bc3aab82def89cd82cf07d5541c8c02d8b
Shougot mollik <shougotmollik@gmail.com>
2023-10-21 21:39:01 +0600

    updated

---
+++ Rivaan Ranawat/Flutter Full Course/flutter_course/lib/currency_convater_material_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConvaterMaterialPage extends StatelessWidget {
+  const CurrencyConvaterMaterialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("Hello World!!")),
+    );
+  }
+}
