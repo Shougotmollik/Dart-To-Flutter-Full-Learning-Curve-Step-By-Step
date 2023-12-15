d02b5a8de4874bec9cfd813d10fa5676178b6eed
Shougot mollik <shougotmollik@gmail.com>
2023-12-15 23:50:11 +0600

    currency conveter

--- Rivaan Ranawat/flutter_full_course_new/lib/currency_converter.dart
+++ Rivaan Ranawat/flutter_full_course_new/lib/currency_converter.dart
@@ -5,6 +5,15 @@ class CurencyConveterMaterial extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const border = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Colors.black,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+    );
     return const Scaffold(
       backgroundColor: Colors.blueGrey,
       body: Center(
@@ -15,21 +24,26 @@ class CurencyConveterMaterial extends StatelessWidget {
               "0",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                color: Colors.white,
+                color: Colors.white60,
                 fontSize: 40,
               ),
             ),
             TextField(
               style: TextStyle(
-                color: Colors.white,
+                color: Colors.black,
               ),
               decoration: InputDecoration(
                 hintText: "Enter The Amount In USD !!",
                 hintStyle: TextStyle(
                   fontWeight: FontWeight.w500,
-                  color: Colors.white60,
+                  color: Colors.black,
                 ),
                 prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Colors.black,
+                filled: true,
+                fillColor: Colors.white,
+                focusedBorder: border,
+                enabledBorder: border,
               ),
             ),
           ],
