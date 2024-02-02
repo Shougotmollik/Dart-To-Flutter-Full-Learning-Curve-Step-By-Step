ba1c55b69f4abd910014e2a5cae66fb39221faba
Shougot mollik <shougotmollik@gmail.com>
2024-02-02 16:37:59 +0600

    loops

---
+++ 100  Days Of Flutter/Dart Programming Language/control statements/loops.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  // for (var i = 0; i < 10; i++) {
+  //   print("i = $i");
+  // }
+
+  int a = 0;
+
+  // while (a < 5) {
+  //   print("Hello world");
+  //   a++;
+  // }
+
+  do {
+    print("hello");
+    a++;
+  } while (a < 5);
+}
