c60ce7f5d3f9c42e70a539b397d5691039a4a2a7
Shougot mollik <shougotmollik@gmail.com>
2024-02-07 22:55:30 +0600

    List view

---
+++ 100  Days Of Flutter/application_one/lib/list_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ListViewApp extends StatelessWidget {
+  const ListViewApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List View"),
+      ),
+    );
+  }
+}
