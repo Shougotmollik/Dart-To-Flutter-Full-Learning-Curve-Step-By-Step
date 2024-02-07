7dcb2b625150c0198eda43053f08111a4bf508b3
Shougot mollik <shougotmollik@gmail.com>
2024-02-07 12:38:39 +0600

    implementation

--- 100  Days Of Flutter/application_one/lib/main.dart
+++ 100  Days Of Flutter/application_one/lib/main.dart
@@ -13,94 +13,90 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int myIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.lime[200],
-        appBar: AppBar(
-          title: const Text('Meassge'),
-          backgroundColor: Colors.lime,
-          actions: [
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.phone))
-          ],
-        ),
-        drawer: Drawer(
           backgroundColor: Colors.lime[200],
-          child: ListView(
-            children: const [
-              DrawerHeader(
-                padding: EdgeInsets.all(0),
-                child: UserAccountsDrawerHeader(
-                    accountName: Text("Shougot"),
-                    accountEmail: Text("Shougot@info.abc")),
-              ),
-              ListTile(
-                leading: Icon(Icons.home),
-                title: Text("Home"),
-              ),
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text("Profile"),
-              ),
-              ListTile(
-                leading: Icon(Icons.mail),
-                title: Text("Email"),
-              ),
-              ListTile(
-                leading: Icon(Icons.phone),
-                title: Text("Phone"),
-              ),
-              ListTile(
-                leading: Icon(Icons.account_balance),
-                title: Text("Balance"),
-              ),
-              ListTile(
-                leading: Icon(Icons.admin_panel_settings),
-              )
+          appBar: AppBar(
+            title: const Text('Meassge'),
+            backgroundColor: Colors.lime,
+            actions: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.phone))
             ],
           ),
-        ),
-        floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.lime[400],
-            onPressed: () {},
-            child: const Icon(Icons.add)),
-        bottomNavigationBar: BottomNavigationBar(
-          backgroundColor: Colors.lime,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.library_books), label: "List"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-          ],
-          currentIndex: myIndex,
-          onTap: (index) {
-            setState(() {
-              myIndex = index;
-            });
-          },
-        ),
-        body: Container(
-          height: 75,
-          width: 400,
-          margin: const EdgeInsets.all(50),
-          padding: const EdgeInsets.all(50),
-          alignment: Alignment.topCenter,
-          decoration: BoxDecoration(
-            color: Colors.orangeAccent,
-            borderRadius: BorderRadius.circular(20),
-            border: const Border(
-              left: BorderSide(
-                color: Colors.green,
-                width: 100,
-              ),
+          drawer: Drawer(
+            backgroundColor: Colors.lime[200],
+            child: ListView(
+              children: const [
+                DrawerHeader(
+                  padding: EdgeInsets.all(0),
+                  child: UserAccountsDrawerHeader(
+                      accountName: Text("Shougot"),
+                      accountEmail: Text("Shougot@info.abc")),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text("Home"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text("Profile"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.mail),
+                  title: Text("Email"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.phone),
+                  title: Text("Phone"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.account_balance),
+                  title: Text("Balance"),
+                ),
+                ListTile(
+                  leading: Icon(Icons.admin_panel_settings),
+                )
+              ],
             ),
           ),
-          child: const Text("Container"),
-        ),
-      ),
+          floatingActionButton: FloatingActionButton(
+              backgroundColor: Colors.lime[400],
+              onPressed: () {},
+              child: const Icon(Icons.add)),
+          bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Colors.lime,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.library_books), label: "List"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person), label: "Profile"),
+            ],
+            currentIndex: myIndex,
+            onTap: (index) {
+              setState(() {
+                myIndex = index;
+              });
+            },
+          ),
+          body: const Text("")),
     );
   }
 }
+
+// myAlertDialog(context) {
+//   return showDialog(
+//       context: context,
+//       builder: (BuildContext context) {
+//         return const Expanded(
+//             child: AlertDialog(
+//           title: Text("Alert"),
+//           content: Text("This is Alert"),
+//         ));
+//       });
+// }
