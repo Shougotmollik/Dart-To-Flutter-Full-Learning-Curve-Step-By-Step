4cf9b71ea99c80be7275e06b2594d04f59ef98b2
Shougot mollik <shougotmollik@gmail.com>
2024-02-19 23:31:50 +0600

    looping

---
+++ App Development with flutter (Ostad)/Introduction with Dart/Intro Dart/looping.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  // for (var i = 0; i <= 100; i++) {
+  //   print('count is $i');
+  //   if (i == 20) {
+  //     break;
+  //   }
+  // }
+
+  int x = 1;
+  while (x <= 10) {
+    print(x);
+    x++;
+  }
+}
