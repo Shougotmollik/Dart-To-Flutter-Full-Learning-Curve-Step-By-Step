56b2165d8f8d4779913654cc0998ce0a724a00d7
Shougot mollik <shougotmollik@gmail.com>
2024-02-19 23:48:13 +0600

    looping

--- App Development with flutter (Ostad)/Introduction with Dart/Intro Dart/looping.dart
+++ App Development with flutter (Ostad)/Introduction with Dart/Intro Dart/looping.dart
@@ -6,9 +6,19 @@ void main(List<String> args) {
   //   }
   // }
 
-  int x = 1;
-  while (x <= 10) {
-    print(x);
-    x++;
+  // int x = 1;
+  // while (x <= 10) {
+  //   print(x);
+  //   x++;
+  // }
+
+  Map<int, String> studentMap = {
+    1: 'shougot',
+    2: 'mollik',
+    3: 'CSE',
+  };
+
+  for (String item in studentMap.values) {
+    print(item);
   }
 }
