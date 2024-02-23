ba11d74f3274b6a087821be488f484f4edb6e8c2
Shougot mollik <shougotmollik@gmail.com>
2024-02-23 23:51:44 +0600

    function

--- App Development with flutter (Ostad)/Introduction with Dart/Intro Dart/function.dart
+++ App Development with flutter (Ostad)/Introduction with Dart/Intro Dart/function.dart
@@ -1,7 +1,8 @@
 void main(List<String> args) {
   // call the function
   // welcomeMessage('shougot', 'Khulna', 22);
-  myFunc(10, 20);
+  // myFunc(10, 20);
+  recursionFunc();
 }
 
 welcomeMessage(String name, [String address = '', int age = 0]) {
@@ -14,4 +15,9 @@ welcomeMsg() {}
 int myFunc(int a, int b) {
   int c = a + b;
   return c;
+}
+
+recursionFunc() {
+  print('This is recusion example');
+  recursionFunc();
 }
