043d164041b50b0477f6c7448f1a4d8065eeeea6
Shougot mollik <shougotmollik@gmail.com>
2024-02-24 00:30:31 +0600

    ternary operator

--- App Development with flutter (Ostad)/Introduction with Dart/Intro Dart/ternary.dart
+++ App Development with flutter (Ostad)/Introduction with Dart/Intro Dart/ternary.dart
@@ -1,5 +1,6 @@
 void main(List<String> args) {
   const isAdult = 20;
-  String adult = (isAdult >= 18) ? 'you are adult' : 'Your are child';
+  String adult =
+      (isAdult >= 18) ? 'you are adult' : 'Your are child'; //True:False
   print(adult);
 }
