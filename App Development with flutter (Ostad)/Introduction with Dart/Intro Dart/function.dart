71ab57b16d6a1fc66cd39880eead93575789e165
Shougot mollik <shougotmollik@gmail.com>
2024-02-23 10:02:53 +0600

    function

--- App Development with flutter (Ostad)/Introduction with Dart/Intro Dart/function.dart
+++ App Development with flutter (Ostad)/Introduction with Dart/Intro Dart/function.dart
@@ -1,6 +1,7 @@
 void main(List<String> args) {
   // call the function
-  welcomeMessage('shougot', 'Khulna', 22);
+  // welcomeMessage('shougot', 'Khulna', 22);
+  myFunc(10, 20);
 }
 
 welcomeMessage(String name, [String address = '', int age = 0]) {
@@ -8,6 +9,9 @@ welcomeMessage(String name, [String address = '', int age = 0]) {
   print('Hello $name $address $age');
 }
 
-welcomeMsg(){
-  
+welcomeMsg() {}
+
+int myFunc(int a, int b) {
+  int c = a + b;
+  return c;
 }
