8b72dbfa04d5c92bb1ea7a6fa68b8d2824cd76c9
Shougot mollik <shougotmollik@gmail.com>
2024-02-26 14:48:34 +0600

    class

--- App Development with flutter (Ostad)/Introduction with Dart/OOPs/class.dart
+++ App Development with flutter (Ostad)/Introduction with Dart/OOPs/class.dart
@@ -1,3 +1,11 @@
-void main(){
-  
+void main() {
+  var MyClassObj = new student();
+  print(MyClassObj.name);
+}
+
+class student {
+  String name = 'shougot';
+  String address = 'Khulna';
+  int age = 21;
+  String department = 'CSE';
 }
