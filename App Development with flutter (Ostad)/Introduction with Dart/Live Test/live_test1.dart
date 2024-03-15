a2c3a3682b99d15e4a1c3ae97d52c58e6022eb7e
Shougot mollik <shougotmollik@gmail.com>
2024-03-15 23:23:22 +0600

    Complete Live Test 1

--- App Development with flutter (Ostad)/Introduction with Dart/Live Test/live_test1.dart
+++ App Development with flutter (Ostad)/Introduction with Dart/Live Test/live_test1.dart
@@ -1,3 +1,36 @@
-void main(){
-  
+void main() {
+  // Map in List
+  List<Map<String, dynamic>> fruits = [
+    {'name': "Apple", 'color': "Red", 'price': 2.5},
+    {'name': "Banana", 'color': "Yellow", 'price': 1.0},
+    {'name': "Grapes", 'color': "Purple", 'price': 3.0}
+  ];
+
+  // Display All fruit orginal Price
+  print("Original Fruit Details before Discount:");
+  displayFruitDetails(fruits);
+
+  // Apply 10% Discount in fruit And Display the Price
+  applyPriceDiscount(fruits, 10);
+
+  print("\nFruit Details After Applying 10% Discount:");
+  displayFruitDetails(fruits);
+}
+
+// DisplayFruitDetails Function
+void displayFruitDetails(fruits) {
+  for (var fruit in fruits) {
+    var fruitDetails =
+        'Name: ${fruit['name']}, Color: ${fruit['color']}, Price: \$${fruit['price']}';
+    print(fruitDetails);
+  }
+}
+
+// applyPriceDiscount Function
+void applyPriceDiscount(fruits, int discount) {
+  for (var fruit in fruits) {
+    double orginalPrice = fruit['price'];
+    double discountedPrice = orginalPrice - (orginalPrice * discount / 100);
+    fruit['price'] = discountedPrice;
+  }
 }
