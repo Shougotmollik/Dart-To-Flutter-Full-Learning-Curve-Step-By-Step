0a4fcb4895c4af689e868dcbda2ad20b297f3253
Shougot mollik <shougotmollik@gmail.com>
2024-03-18 00:41:57 +0600

    Start New Project

---
+++ application_one/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:application_one/screens/home_page.dart';
+import 'package:application_one/screens/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
