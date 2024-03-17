7d2e66a4a11d0bb4c28bf6b427424bbe2612ad11
Shougot mollik <shougotmollik@gmail.com>
2024-03-18 00:41:48 +0600

    Start New Project

--- App Development with flutter (Ostad)/Flutter Essentials/application_one/android/app/src/main/kotlin/com/example/application_one/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.application_one
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ application_one/lib/screens/login_page.dart
@@ -0,0 +1,91 @@
+import 'dart:ui';
+
+import 'package:application_one/screens/home_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 32,
+            ),
+            Image.asset(
+              'assets/images/login.png',
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "Email Or Phone Number",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepPurpleAccent,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+              ),
+              child: const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  "Login",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    color: Colors.white,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ),
+            ),
+            const InkWell(
+              child: Text('SingUp'),
+              onLongPress: ,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
