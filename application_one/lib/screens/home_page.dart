cdda37c8eb0a2d161ff45ad9f42bbdc6124956a4
Shougot mollik <shougotmollik@gmail.com>
2024-03-18 15:05:08 +0600

    Home page

--- application_one/lib/screens/home_page.dart
+++ application_one/lib/screens/home_page.dart
@@ -5,6 +5,37 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        backgroundColor: Colors.deepPurple[400],
+      ),
+      drawer: const Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              accountEmail: Text("abc@gmail.com"),
+              accountName: Text("Abc"),
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+              ),
+            ),
+            ListTile(
+              title: Text('Profile'),
+              leading: Icon(Icons.person),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.deepPurple[200],
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.book), label: "Courses"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
   }
 }
