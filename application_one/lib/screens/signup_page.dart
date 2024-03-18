1eab3e52741acfd3f7a8647cc0fdd505e5ee3b92
Shougot mollik <shougotmollik@gmail.com>
2024-03-18 14:30:35 +0600

    sign up page

--- application_one/lib/screens/signup_page.dart
+++ application_one/lib/screens/signup_page.dart
@@ -1,10 +1,64 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class SignupPage extends StatelessWidget {
   const SignupPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Sign Up"),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Enter Your Name",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Enter Username",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Enter Password",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
