359b070de7173830e9ce8b862157fd88e683a659
Shougot mollik <shougotmollik@gmail.com>
2024-03-19 15:34:58 +0600

    home pages

--- application_one/lib/screens/home_page.dart
+++ application_one/lib/screens/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,36 +8,34 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple[100],
-      appBar: AppBar(
-        title: const Text("Home Page"),
-        backgroundColor: Colors.deepPurple[400],
-      ),
-      drawer: const Drawer(
-        child: Column(
-          children: [
-            UserAccountsDrawerHeader(
-              accountEmail: Text("abc@gmail.com"),
-              accountName: Text("Abc"),
-              decoration: BoxDecoration(
-                color: Colors.deepPurple,
-              ),
-            ),
-            ListTile(
-              title: Text('Profile'),
-              leading: Icon(Icons.person),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text("Home Page"),
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.deepPurple[200],
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.book), label: "Courses"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-        ],
-      ),
-    );
+        drawer: const Drawer(
+          child: Column(
+            children: [
+              UserAccountsDrawerHeader(
+                accountEmail: Text("abc@gmail.com"),
+                accountName: Text("Abc"),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                ),
+              ),
+              ListTile(
+                title: Text('Profile'),
+                leading: Icon(Icons.person),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.deepPurpleAccent,
+          unselectedItemColor: Colors.black54,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+          ],
+        ));
   }
 }
