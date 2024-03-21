96de6378a6daecc33809e8f1041cac169ebc36c4
Shougot mollik <shougotmollik@gmail.com>
2024-03-21 19:01:00 +0600

    implementation

--- application_one/lib/main.dart
+++ application_one/lib/main.dart
@@ -10,10 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.deepOrange),
-      home: const LoginPage(),
+    return GestureDetector(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(primarySwatch: Colors.deepOrange),
+        home: const LoginPage(),
+      ),
     );
   }
 }
