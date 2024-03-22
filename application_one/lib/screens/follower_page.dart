eb3fe905e6e6721d0e9b703cb8d32b881c24c3a2
Shougot mollik <shougotmollik@gmail.com>
2024-03-22 14:49:45 +0600

    follower  Page

---
+++ application_one/lib/screens/follower_page.dart
@@ -0,0 +1,52 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class FollowerPage extends StatefulWidget {
+  const FollowerPage({super.key});
+
+  @override
+  State<FollowerPage> createState() => _FollowerPageState();
+}
+
+class _FollowerPageState extends State<FollowerPage> {
+  int follower = 10;
+  increaseFollower() {
+    setState(() {
+      follower += 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Follower page"),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(
+                "https://img.freepik.com/premium-vector/young-man-animestyle-character-vector-illustration-design-manga-anime-boy_147933-2347.jpg?size=338&ext=jpg&ga=GA1.1.1700460183.1708214400&semt=ais",
+              ),
+            ),
+          ),
+          const Text("Follower "),
+          Text(follower.toString()),
+          ElevatedButton.icon(
+            style: const ButtonStyle(),
+            onPressed: () {
+              increaseFollower();
+            },
+            icon: const Icon(Icons.add),
+            label: const Text("Follow"),
+          ),
+        ],
+      ),
+    );
+  }
+}
