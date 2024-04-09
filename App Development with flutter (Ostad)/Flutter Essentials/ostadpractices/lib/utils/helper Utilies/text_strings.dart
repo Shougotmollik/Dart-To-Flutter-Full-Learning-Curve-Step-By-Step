4518e8efd22e93a2fa0994503371f8ecef8193b3
Shougot mollik <shougotmollik@gmail.com>
2024-04-09 22:02:16 +0600

    text strings

---
+++ App Development with flutter (Ostad)/Flutter Essentials/ostadpractices/lib/utils/helper Utilies/text_strings.dart
@@ -0,0 +1,16 @@
+class AppTextStrings {
+  AppTextStrings._();
+
+//   Onboarding Text
+
+  static const String onboardingTitle1 = "Select Your Choice";
+  static const String onboardingTitle2 = "Secure Payment";
+  static const String onboardingTitle3 = "Delivery on door Step";
+
+  static const String onboardingSubtitle1 =
+      " every time you explorer somethings new and make you life style as you wised";
+  static const String onboardingSubtitle2 =
+      " Pay your bill like a credit transfer or send money some one";
+  static const String onboardingSubtitle3 =
+      "Delivery at you address and enjoy your life tension free";
+}
