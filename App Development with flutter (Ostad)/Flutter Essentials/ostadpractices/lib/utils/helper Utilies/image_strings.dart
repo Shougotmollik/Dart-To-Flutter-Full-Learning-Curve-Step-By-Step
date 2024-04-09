3e0661efe8a5f9c42d7b771d7e0f8040a18ceed4
Shougot mollik <shougotmollik@gmail.com>
2024-04-09 22:02:54 +0600

    image strings

---
+++ App Development with flutter (Ostad)/Flutter Essentials/ostadpractices/lib/utils/helper Utilies/image_strings.dart
@@ -0,0 +1,12 @@
+class AppImageStrings {
+  AppImageStrings._();
+
+//   Onboarding Strings
+
+  static const String onBoardingImage1 =
+      "assets/images/onboarding_images/choose.gif";
+  static const String onBoardingImage2 =
+      "assets/images/onboarding_images/payment.gif";
+  static const String onBoardingImage3 =
+      "assets/images/onboarding_images/delivery.gif";
+}
