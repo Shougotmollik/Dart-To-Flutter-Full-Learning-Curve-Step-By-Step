f4de843c2b605aab06fe59e5c01f9b351a831de8
Shougot mollik <shougotmollik@gmail.com>
2024-04-26 01:41:43 +0600

    home screen

--- App Development with flutter (Ostad)/Fltter Assignment/galary_app/lib/Screens/HomeScreen/home_screen.dart
+++ App Development with flutter (Ostad)/Fltter Assignment/galary_app/lib/Screens/HomeScreen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:galary_app/Widgets/custom_appbar.dart';
+import 'package:gallery_app/Models/images_model.dart';
+import 'package:gallery_app/Widgets/custom_appbar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,8 +12,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(),
+    return Scaffold(
+      appBar: const CustomAppBar(
+        titleText: "Photo Gallery",
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 18),
+        child: GridView.builder(
+          itemCount: imageDetails.length,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemBuilder: (context, index) {
+            return InkWell(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Stack(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height * 0.21,
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage(
+                              "${imageDetails.elementAt(index)["image"]}"),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 120,
+                      left: 45,
+                      child: Text(
+                        "${imageDetails.elementAt(index)["label"]}",
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
