d96123097f6ec4cac7f3b29fc354514fd115bab4
Shougot mollik <shougotmollik@gmail.com>
2024-04-28 01:12:43 +0600

    details page

--- App Development with flutter (Ostad)/Fltter Assignment/galary_app/lib/Screens/Detailscreen/details_screen.dart
+++ App Development with flutter (Ostad)/Fltter Assignment/galary_app/lib/Screens/Detailscreen/details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gallery_app/Models/images_model.dart';
+import 'package:gallery_app/Models/data_model.dart';
+import 'package:gallery_app/Utils/image_strings.dart';
 import 'package:gallery_app/Widgets/custom_appbar.dart';
 
 class DetailsScreen extends StatelessWidget {
@@ -12,6 +13,145 @@ class DetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(
         titleText: getAppDataDetails.label,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.38,
+                width: MediaQuery.of(context).size.width * 3,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  image: DecorationImage(
+                    image: AssetImage(getAppDataDetails.images),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    getAppDataDetails.bodyTitle,
+                    style: const TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  Text(
+                    getAppDataDetails.description,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                  const SizedBox(height: 18),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 3,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).colorScheme.primary,
+                        borderRadius: BorderRadius.circular(100)),
+                    child: TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "See More",
+                        style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                            color: Theme.of(context).colorScheme.secondary),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 18),
+                    child: Text(
+                      "Suggestions",
+                      style: TextStyle(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 19, right: 12),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.18,
+                        width: MediaQuery.of(context).size.width * 0.42,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: const DecorationImage(
+                            image: AssetImage(AppImages.dawn),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        top: 120,
+                        left: 35,
+                        child: Text(
+                          "Dawn",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 14, right: 19),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.18,
+                        width: MediaQuery.of(context).size.width * 0.42,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: const DecorationImage(
+                            image: AssetImage(AppImages.leaves),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        top: 120,
+                        left: 35,
+                        child: Text(
+                          "Leaves",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
