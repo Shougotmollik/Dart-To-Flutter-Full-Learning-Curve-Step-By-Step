cd3433ce0e9a7786f7ed2d5e6c1ce805c47f9bb1
Shougot mollik <shougotmollik@gmail.com>
2024-04-27 23:27:38 +0600

    data model

--- App Development with flutter (Ostad)/Fltter Assignment/galary_app/lib/Models/images_model.dart
+++ App Development with flutter (Ostad)/Fltter Assignment/galary_app/lib/Models/images_model.dart
@@ -1,44 +1,67 @@
 import 'package:gallery_app/Utils/image_strings.dart';
 
-final List<Map<String, dynamic>> imageDetails = [
-  {
-    "image": AppImages.mood,
-    "label": "Mood",
-  },
-  {
-    "image": AppImages.aesthetic,
-    "label": "Aesthetic",
-  },
-  {
-    "image": AppImages.animal,
-    "label": "Animal",
-  },
-  {
-    "image": AppImages.city,
-    "label": "City",
-  },
-  {
-    "image": AppImages.dawn,
-    "label": "Dawn",
-  },
-  {
-    "image": AppImages.flowers,
-    "label": "Flowers",
-  },
-  {
-    "image": AppImages.leaves,
-    "label": "Leaves",
-  },
-  {
-    "image": AppImages.road,
-    "label": "Road",
-  },
-  {
-    "image": AppImages.sky,
-    "label": "Sky",
-  },
-  {
-    "image": AppImages.travel,
-    "label": "Travel",
-  },
+class AppData {
+  final String images;
+  final String label;
+  final String description;
+
+  const AppData({
+    required this.images,
+    required this.label,
+    required this.description,
+  });
+}
+
+List<AppData> appData = [
+  const AppData(
+    images: AppImages.mood,
+    label: 'Mood',
+    description:
+        " Being in nature, or even viewing scenes of nature, reduces anger, fear, and stress and increases pleasant feelings",
+  ),
+  const AppData(
+    images: AppImages.aesthetic,
+    label: 'Aesthetic',
+    description: " loldodlsdoldsodosdjsdkdmk",
+  ),
+  const AppData(
+    images: AppImages.animal,
+    label: 'Animal',
+    description: " loldodlsdoldsodosdjsdkdmk",
+  ),
+  const AppData(
+    images: AppImages.city,
+    label: 'City',
+    description: " loldodlsdoldsodosdjsdkdmk",
+  ),
+  const AppData(
+    images: AppImages.travel,
+    label: 'Travel',
+    description: " loldodlsdoldsodosdjsdkdmk",
+  ),
+  const AppData(
+    images: AppImages.sky,
+    label: 'Sky',
+    description: " loldodlsdoldsodosdjsdkdmk",
+  ),
+  const AppData(
+    images: AppImages.road,
+    label: 'Road',
+    description: " loldodlsdoldsodosdjsdkdmk",
+  ),
+  const AppData(
+    images: AppImages.flowers,
+    label: 'Flowers',
+    description: " loldodlsdoldsodosdjsdkdmk",
+  ),
+  const AppData(
+    images: AppImages.dawn,
+    label: 'Dawn',
+    description: " loldodlsdoldsodosdjsdkdmk",
+  ),
+  const AppData(
+    images: AppImages.leaves,
+    label: 'Leaves',
+    description: " loldodlsdoldsodosdjsdkdmk",
+  ),
 ];
