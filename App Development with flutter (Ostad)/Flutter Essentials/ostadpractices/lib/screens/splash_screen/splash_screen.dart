017bd102da4fec487fb1afc217d8769e85620621
Shougot mollik <shougotmollik@gmail.com>
2024-05-07 21:43:59 +0600

    splash screen

--- App Development with flutter (Ostad)/Flutter Essentials/ostadpractices/lib/screens/splash_screen/splash_screen.dart
+++ App Development with flutter (Ostad)/Flutter Essentials/ostadpractices/lib/screens/splash_screen/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:ostadpractices/screens/HomeScreen/home_page.dart';
+import 'package:ostadpractices/utils/helper%20Utilies/image_strings.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,7 +12,29 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const HomePage(),
+            ));
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset(AppImageStrings.onBoardingImage2)],
+        ),
+      ),
+    );
   }
 }
