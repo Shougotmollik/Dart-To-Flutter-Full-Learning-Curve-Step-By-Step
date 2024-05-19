8570a0608dd2cce028a96b519bb5ff9b45fe28cc
Shougot mollik <shougotmollik@gmail.com>
2024-05-19 11:36:58 +0600

    details screen

--- App Development with flutter (Ostad)/Fltter Assignment/galary_app_api/lib/Screens/DetailsScreen/details_screen.dart
+++ App Development with flutter (Ostad)/Fltter Assignment/galary_app_api/lib/Screens/DetailsScreen/details_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:galary_app_api/Model/model.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({super.key});
+  const DetailsScreen(
+    List<Product> product, {
+    super.key,
+  });
+ 
 
   @override
   Widget build(BuildContext context) {
@@ -16,11 +21,48 @@ class DetailsScreen extends StatelessWidget {
           ),
         ),
         leading: IconButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.pop(context);
+          },
           icon: const Icon(
             Icons.arrow_back,
             color: Colors.white,
           ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.6,
+              width: MediaQuery.of(context).size.width * double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+            Column(
+              children: [
+                Text(
+                  "",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                  ),
+                ),
+                Text(
+                  "ID: ",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
