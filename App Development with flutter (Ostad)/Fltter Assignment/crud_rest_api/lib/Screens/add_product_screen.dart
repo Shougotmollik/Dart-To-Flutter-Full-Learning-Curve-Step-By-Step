70c88502b58fa6531cbf67d11d6f2ebdeb749aae
Shougot mollik <shougotmollik@gmail.com>
2024-05-25 17:05:43 +0600

    add product

--- App Development with flutter (Ostad)/Fltter Assignment/crud_rest_api/lib/Screens/add_product_screen.dart
+++ App Development with flutter (Ostad)/Fltter Assignment/crud_rest_api/lib/Screens/add_product_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class AddProductScreen extends StatefulWidget {
   const AddProductScreen({super.key});
@@ -8,71 +11,180 @@ class AddProductScreen extends StatefulWidget {
 }
 
 class _AddProductScreenState extends State<AddProductScreen> {
-  final TextEditingController _pNameTEController = TextEditingController();
+  final TextEditingController _nameTEController = TextEditingController();
   final TextEditingController _unitPriceTEController = TextEditingController();
   final TextEditingController _quantityTEController = TextEditingController();
   final TextEditingController _totalPriceTEController = TextEditingController();
   final TextEditingController _imageTEController = TextEditingController();
+  final TextEditingController _productCodeTEController =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  bool _addNewProductInProgress = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Add Product"),
+        title: const Text('Add New Product'),
       ),
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: _pNameTEController,
-                decoration: const InputDecoration(
-                    hintText: "Product Name", labelText: "Product Name"),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _unitPriceTEController,
-                decoration: const InputDecoration(
-                    hintText: "Unit Price", labelText: "Unit Price"),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _quantityTEController,
-                decoration: const InputDecoration(
-                    hintText: "Quantity ", labelText: "Quantity"),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _totalPriceTEController,
-                decoration: const InputDecoration(
-                    hintText: "Total Price", labelText: "Total Price"),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _imageTEController,
-                decoration: const InputDecoration(
-                    hintText: "Image", labelText: "Image"),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("App Product"),
-              )
-            ],
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nameTEController,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: const InputDecoration(
+                      hintText: 'Name', labelText: 'Name'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your product name';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _unitPriceTEController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'Unit Price', labelText: 'Unit Price'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your unit price';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _productCodeTEController,
+                  keyboardType: TextInputType.text,
+                  decoration: const InputDecoration(
+                      hintText: 'Product Code', labelText: 'Product Code'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your product code';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _quantityTEController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'Quantity', labelText: 'Quantity'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your quantity';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _totalPriceTEController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'Total Price', labelText: 'Total Price'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your total price';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _imageTEController,
+                  decoration: const InputDecoration(
+                      hintText: 'Image', labelText: 'Image'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your image';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                Visibility(
+                  visible: _addNewProductInProgress == false,
+                  replacement: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _addProduct();
+                      }
+                    },
+                    child: const Text('Add'),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
+  Future<void> _addProduct() async {
+    _addNewProductInProgress = true;
+    setState(() {});
+
+    const String addNewProductUrl =
+        'https://crud.teamrabbil.com/api/v1/CreateProduct';
+
+    Map<String, dynamic> inputData = {
+      "Img": _imageTEController.text.trim(),
+      "ProductCode": _productCodeTEController.text,
+      "ProductName": _nameTEController.text,
+      "Qty": _quantityTEController.text,
+      "TotalPrice": _totalPriceTEController.text,
+      "UnitPrice": _unitPriceTEController.text
+    };
+
+    Uri uri = Uri.parse(addNewProductUrl);
+
+    Response response = await post(
+      uri,
+      body: jsonEncode(inputData),
+      headers: {'content-type': 'application/json'},
+    );
+
+    _addNewProductInProgress = false;
+    setState(() {});
+
+    if (response.statusCode == 200) {
+      _nameTEController.clear();
+      _unitPriceTEController.clear();
+      _productCodeTEController.clear();
+      _quantityTEController.clear();
+      _totalPriceTEController.clear();
+      _imageTEController.clear();
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('New product added!')),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Add new product failed! Try again.')),
+      );
+    }
+  }
+
   @override
   void dispose() {
+    _nameTEController.dispose();
+    _unitPriceTEController.dispose();
+    _quantityTEController.dispose();
+    _totalPriceTEController.dispose();
+    _imageTEController.dispose();
     super.dispose();
-    _pNameTEController;
-    _quantityTEController;
-    _totalPriceTEController;
-    _unitPriceTEController;
-    _imageTEController;
   }
 }
