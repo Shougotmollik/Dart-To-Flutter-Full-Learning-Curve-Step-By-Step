5b96c2f5f57417fc09d5a03431c1a6235d64c8a3
Shougot mollik <shougotmollik@gmail.com>
2024-05-25 17:06:20 +0600

    update product screen

--- App Development with flutter (Ostad)/Fltter Assignment/crud_rest_api/lib/Screens/update_product_screen.dart
+++ App Development with flutter (Ostad)/Fltter Assignment/crud_rest_api/lib/Screens/update_product_screen.dart
@@ -1,76 +1,192 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+import '../Model/product_model.dart';
 
 class UpdateProductScreen extends StatefulWidget {
-  const UpdateProductScreen({super.key});
+  const UpdateProductScreen({super.key, required this.product});
+
+  final ProductModel product;
 
   @override
   State<UpdateProductScreen> createState() => _UpdateProductScreenState();
 }
 
 class _UpdateProductScreenState extends State<UpdateProductScreen> {
-  final TextEditingController _pNameTEController = TextEditingController();
+  final TextEditingController _nameTEController = TextEditingController();
   final TextEditingController _unitPriceTEController = TextEditingController();
   final TextEditingController _quantityTEController = TextEditingController();
   final TextEditingController _totalPriceTEController = TextEditingController();
   final TextEditingController _imageTEController = TextEditingController();
+  final TextEditingController _productCodeTEController =
+      TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _updateProductInProgress = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _nameTEController.text = widget.product.productName ?? '';
+    _unitPriceTEController.text = widget.product.unitPrice ?? '';
+    _quantityTEController.text = widget.product.quantity ?? '';
+    _totalPriceTEController.text = widget.product.totalPrice ?? '';
+    _imageTEController.text = widget.product.image ?? '';
+    _productCodeTEController.text = widget.product.productCode ?? '';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Update Product"),
+        title: const Text('Update Product'),
       ),
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            children: [
-              TextField(
-                controller: _pNameTEController,
-                decoration: const InputDecoration(
-                    hintText: "Product Name", labelText: "Product Name"),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _unitPriceTEController,
-                decoration: const InputDecoration(
-                    hintText: "Unit Price", labelText: "Unit Price"),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _quantityTEController,
-                decoration: const InputDecoration(
-                    hintText: "Quantity ", labelText: "Quantity"),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _totalPriceTEController,
-                decoration: const InputDecoration(
-                    hintText: "Total Price", labelText: "Total Price"),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _imageTEController,
-                decoration: const InputDecoration(
-                    hintText: "Image", labelText: "Image"),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text("App Product"),
-              )
-            ],
+          padding: const EdgeInsets.all(16),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                TextFormField(
+                  controller: _nameTEController,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  decoration: const InputDecoration(
+                      hintText: 'Name', labelText: 'Name'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your product name';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _unitPriceTEController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'Product Code', labelText: 'Product Code'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your product code';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _productCodeTEController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'Unit Price', labelText: 'Unit Price'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your unit price';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _quantityTEController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'Quantity', labelText: 'Quantity'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your quantity';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _totalPriceTEController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                      hintText: 'Total Price', labelText: 'Total Price'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your total price';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _imageTEController,
+                  decoration: const InputDecoration(
+                      hintText: 'Image', labelText: 'Image'),
+                  validator: (String? value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Write your image';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                Visibility(
+                  visible: _updateProductInProgress == false,
+                  replacement: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _updateProduct();
+                      }
+                    },
+                    child: const Text('Update'),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
+  Future<void> _updateProduct() async {
+    _updateProductInProgress = true;
+    setState(() {});
+
+    Map<String, String> inputData = {
+      "Img": _imageTEController.text,
+      "ProductCode": _productCodeTEController.text,
+      "ProductName": _nameTEController.text,
+      "Qty": _quantityTEController.text,
+      "TotalPrice": _totalPriceTEController.text,
+      "UnitPrice": _unitPriceTEController.text
+    };
+
+    String updateProductUrl =
+        'https://crud.teamrabbil.com/api/v1/UpdateProduct/${widget.product.id}';
+    Uri uri = Uri.parse(updateProductUrl);
+    Response response = await post(uri,
+        headers: {'content-type': 'application/json'},
+        body: jsonEncode(inputData));
+
+    if (response.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Product has been updated')),
+      );
+      Navigator.pop(context, true);
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Update product failed! Try again.')),
+      );
+    }
+  }
+
+  @override
   void dispose() {
+    _nameTEController.dispose();
+    _unitPriceTEController.dispose();
+    _quantityTEController.dispose();
+    _totalPriceTEController.dispose();
+    _imageTEController.dispose();
     super.dispose();
-    _pNameTEController;
-    _quantityTEController;
-    _totalPriceTEController;
-    _unitPriceTEController;
-    _imageTEController;
   }
 }
