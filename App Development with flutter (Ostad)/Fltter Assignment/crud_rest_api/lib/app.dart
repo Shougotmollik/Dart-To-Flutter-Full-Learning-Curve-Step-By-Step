cb61d3574d35ea6013a452aa2ee0487482ab55fe
Shougot mollik <shougotmollik@gmail.com>
2024-05-25 17:05:50 +0600

    app

--- App Development with flutter (Ostad)/Fltter Assignment/crud_rest_api/lib/app.dart
+++ App Development with flutter (Ostad)/Fltter Assignment/crud_rest_api/lib/app.dart
@@ -10,25 +10,23 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
       theme: ThemeData(
-        brightness: Brightness.dark,
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
+          brightness: Brightness.dark,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
           ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-                  fixedSize: const Size.fromWidth(double.maxFinite),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-        )
-      ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              fixedSize: const Size.fromWidth(double.maxFinite),
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+          )),
       title: 'Crud App api',
-      home: const ProductPage(),
+      home: const ProductScreen(),
     );
   }
 }
