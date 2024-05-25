0c0d1c78544fa510e5fde97df4365c769de9cac6
Shougot mollik <shougotmollik@gmail.com>
2024-05-25 17:06:10 +0600

    product screen

--- App Development with flutter (Ostad)/Fltter Assignment/crud_rest_api/lib/Screens/product_screen.dart
+++ App Development with flutter (Ostad)/Fltter Assignment/crud_rest_api/lib/Screens/product_screen.dart
@@ -1,58 +1,177 @@
-import 'package:crud_rest_api/Screens/add_product_screen.dart';
+import 'dart:convert';
+
 import 'package:crud_rest_api/Screens/update_product_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
-class ProductPage extends StatefulWidget {
-  const ProductPage({super.key});
+import '../Model/product_model.dart';
+import 'add_product_screen.dart';
+
+class ProductScreen extends StatefulWidget {
+  const ProductScreen({super.key});
 
   @override
-  State<ProductPage> createState() => _ProductPageState();
+  State<ProductScreen> createState() => _ProductScreenState();
 }
 
-class _ProductPageState extends State<ProductPage> {
+class _ProductScreenState extends State<ProductScreen> {
+  bool _getProductListInProgress = false;
+  List<ProductModel> productList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getProductList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Product List"),
+        title: const Text('Product list'),
       ),
-      body: ListView.separated(
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: const Text("Product name"),
-              subtitle: const Wrap(
-                spacing: 22,
-                children: [Text("Quantity"), Text("Price")],
-              ),
-              trailing: Wrap(
-                children: [
-                  IconButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const UpdateProductScreen(),
-                          ),
-                        );
-                      },
-                      icon: const Icon(Icons.edit)),
-                  IconButton(onPressed: () {}, icon: const Icon(Icons.delete))
-                ],
-              ),
-            );
-          },
-          separatorBuilder: (context, index) => const Divider(),
-          itemCount: 5),
+      body: RefreshIndicator(
+        onRefresh: _getProductList,
+        child: Visibility(
+          visible: _getProductListInProgress == false,
+          replacement: const Center(
+            child: CircularProgressIndicator(),
+          ),
+          child: ListView.separated(
+            itemCount: productList.length,
+            itemBuilder: (context, index) {
+              return _buildProductItem(productList[index]); // n(1)
+            },
+            separatorBuilder: (_, __) => const Divider(),
+          ),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const AddProductScreen(),
-              ));
+            context,
+            MaterialPageRoute(builder: (context) => const AddProductScreen()),
+          );
         },
         child: const Icon(Icons.add),
       ),
     );
+  }
+
+  Future<void> _getProductList() async {
+    _getProductListInProgress = true;
+    setState(() {});
+    productList.clear();
+    const String productListUrl =
+        'https://crud.teamrabbil.com/api/v1/ReadProduct';
+    Uri uri = Uri.parse(productListUrl);
+    Response response = await get(uri);
+
+    if (response.statusCode == 200) {
+      final decodedData = jsonDecode(response.body);
+
+      final jsonProductList = decodedData['data'];
+
+      for (Map<String, dynamic> json in jsonProductList) {
+        ProductModel productModel = ProductModel.fromJson(json);
+        productList.add(productModel);
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Get product list failed! Try again.')),
+      );
+    }
+
+    _getProductListInProgress = false;
+    setState(() {});
+  }
+
+  Widget _buildProductItem(ProductModel product) {
+    return ListTile(
+      title: Text(product.productName ?? 'Unknown'),
+      subtitle: Wrap(
+        spacing: 16,
+        children: [
+          Text('Unit Price: ${product.unitPrice}'),
+          Text('Quantity : ${product.quantity}'),
+          Text('Total Price: ${product.totalPrice}'),
+        ],
+      ),
+      trailing: Wrap(
+        children: [
+          IconButton(
+            icon: const Icon(Icons.edit),
+            onPressed: () async {
+              final result = await Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UpdateProductScreen(
+                    product: product,
+                  ),
+                ),
+              );
+              if (result == true) {
+                _getProductList();
+              }
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.delete_outline_sharp),
+            onPressed: () {
+              _showDeleteConfirmationDialog(product.id!);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showDeleteConfirmationDialog(String productId) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Delete'),
+          content:
+              const Text('Are you sure that you want to delete this product?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () {
+                _deleteProduct(productId);
+                Navigator.pop(context);
+              },
+              child: const Text('Yes, delete'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> _deleteProduct(String productId) async {
+    _getProductListInProgress = true;
+    setState(() {});
+    String deleteProductUrl =
+        'https://crud.teamrabbil.com/api/v1/DeleteProduct/$productId';
+    Uri uri = Uri.parse(deleteProductUrl);
+    Response response = await get(uri);
+    print(response.statusCode);
+    print(response.body);
+
+    if (response.statusCode == 200) {
+      _getProductList();
+    } else {
+      _getProductListInProgress = false;
+      setState(() {});
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Delete product failed! Try again.')),
+      );
+    }
   }
 }
