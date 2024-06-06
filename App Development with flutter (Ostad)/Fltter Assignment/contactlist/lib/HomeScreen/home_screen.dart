b0e30b759a4d4e4946c88299943e1de69a75e720
ShougotMollik <shougotmollik@gmail.com>
2024-06-07 00:50:13 +0600

    complete the assignment

--- App Development with flutter (Ostad)/Fltter Assignment/contactlist/lib/HomeScreen/home_screen.dart
+++ App Development with flutter (Ostad)/Fltter Assignment/contactlist/lib/HomeScreen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:contactlist/Config/model/contact.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +9,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController numberController = TextEditingController();
+  List<Contact> contact = List.empty(growable: true);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +28,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           children: [
             TextField(
+              controller: nameController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
@@ -32,6 +38,8 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             const SizedBox(height: 18),
             TextField(
+              controller: numberController,
+              keyboardType: TextInputType.number,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
@@ -51,10 +59,93 @@ class _HomeScreenState extends State<HomeScreen> {
                   "ADD",
                   style: TextStyle(color: Colors.white),
                 ),
-                onPressed: () {},
+                onPressed: () {
+                  String name = nameController.text.trim();
+                  String number = numberController.text.trim();
+                  if (name.isNotEmpty && number.isNotEmpty) {
+                    contact.add(Contact(name: name, number: number));
+                    nameController.clear();
+                    numberController.clear();
+                    setState(() {});
+                  }
+                },
               ),
-            )
+            ),
+            const SizedBox(height: 18),
+            contact.isEmpty
+                ? const Text(
+                    "No contact are available !!!",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 18,
+                    ),
+                  )
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: contact.length,
+                      itemBuilder: (context, index) => getContactData(index),
+                    ),
+                  )
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget getContactData(int index) {
+    return GestureDetector(
+      onLongPress: () {
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text(
+              "Confirmation",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            content: const Text("Are you sure for Delete ?"),
+            actions: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.save)),
+              IconButton(
+                  onPressed: () {
+                    contact.removeAt(index);
+                    Navigator.pop(context);
+
+                    setState(() {});
+                  },
+                  icon: const Icon(Icons.delete)),
+            ],
+          ),
+        );
+      },
+      child: Card(
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 28,
+            backgroundColor: index % 2 == 0
+                ? Colors.deepOrangeAccent
+                : Colors.lightBlueAccent,
+            foregroundColor: Colors.white,
+            child: Text(contact[index].name[0]),
+          ),
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                contact[index].name,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                contact[index].number,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+          trailing: IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
         ),
       ),
     );
