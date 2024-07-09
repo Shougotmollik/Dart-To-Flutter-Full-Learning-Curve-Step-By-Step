f5b732af75f674ee711dcc9458b40e9b3b8ba221
Shougot mollik <shougotmollik@gmail.com>
2024-07-09 16:30:26 +0600

    start new project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:getx_application/app.dart';
 
-import 'package:getx_application/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
